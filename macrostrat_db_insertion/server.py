f991bce838d88923a7541c2206f18684b17193c5
David Sklar <davidsklar99@outlook.com>
2026-09-22 13:09:22 -0500

    Allow dev access

--- macrostrat_db_insertion/server.py
+++ macrostrat_db_insertion/server.py
@@ -41,10 +41,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://macrostrat.org",
+        "https://dev.macrostrat.org",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 def get_complete_table_name(table_name):
